ed36ee8ad30a8f492c5dc11acfed8f31854131f8
mayrahhhh <128723776+mayrahhhh@users.noreply.github.com>
2023-10-19 09:52:03 -0500

    Add files via upload

--- despliegue.php
+++ despliegue.php
@@ -3,5 +3,13 @@
 //se incluye el archivo funciones
 include("funciones.php");
 
+
 //se invoca la funcion de consulta
-echo consulta();
+
+//echo consulta();
+
+//echo aprende();
+
+//echo calculo_version2();
+
+echo conteo();
--- funciones.php
+++ funciones.php
@@ -1,25 +1,60 @@
 <?php
+//es lo que aparecera en el navegador mediante el include
+//echo "hola";
 
-//se imprime hola en funciones.php
-
-function consulta(){
-
-    $salida=""; //inicializa la variable
-    $salida=10*2/2;  //calcula el area del triangulo
-    $conexion= mysqli_connect("localhost","root","root","db_factura");//conexion hacia la base de datos
-    $sql="select 2+1 as sum";//seleccion de una suma en sql
-
-
-
-
-
-
-
-
-
-//retorna la operacion
-    return $salida;
+function consulta(){ // nombre de la funcion
+    $salida=0; //inicializa la variable
+    $salida=10*2/2; //calcula el area del triangulo
+    $conexion=mysqli_connect("localhost","root","root","db_factura"); //conexion con el servidor y la base de datos
+    $sql="SELECT 20+10 "; //slecciona algo de la base de datos
+    $sql.="as suma"; //selecciona algo de la base de tados
+    $resultado =$conexion->query($sql); //ejecuta una consulta
+    while($fila=mysqli_fetch_assoc($resultado)) //recorre el recordset
+{
+    $salida+=$fila['suma']; //inicio la columna
 }
+    return $salida; //retorna la operación
+}
+
+function aprender(){ //nombre de la funcion
+    $salida=0; //inicializacion
+    $salida=2+45; //operacion de suma
+
+    return $salida; //retorna la operación
+}
+function calculo_version2() {
+    $salida = 0;//inicializacion de la variable
+    $conexion = mysqli_connect("localhost", "root", "root", "db_factura");//cpnexion de la base de datos
+    $sql = "SELECT 10 as n1, 20 as n2";
+    $resultado = $conexion->query($sql); //ejecuta una consulta
+
+    while ($fila = mysqli_fetch_assoc($resultado)) {//recoge el recorset
+        $salida += $fila['n1']; // Acumula el valor de 'n1'
+        $salida += $fila['n2']; // Acumula el valor de 'n2'
+    }
+
+    return $salida;//retorna la operación
+}
+
+
+
+function conteo() {//nombre de la funcion
+    $salida = 0;//inicializacion de la variable
+    $conexion = mysqli_connect("localhost", "root", "root", "db_factura"); // Conexión a la base de datos
+    $sql = "SELECT COUNT(*) as usuario_count FROM usuario"; // Consulta para contar el número de registros en la tabla 'usuarios'
+    $resultado = $conexion->query($sql);// la variable sql, ejecuta una consulta
+
+    if ($resultado) {
+        $fila = $resultado->fetch_assoc();//recoge el recorset
+        $salida = $fila['usuario_count'];//inicio la columna
+    } else {
+      
+    }
+
+    return $salida;//retorna la operación
+}
+
+
 
 
 
