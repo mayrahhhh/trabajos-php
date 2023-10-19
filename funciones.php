9c7337f4474b5e954a3574da6dc3591f4d8c2d72
mayrahhhh <128723776+mayrahhhh@users.noreply.github.com>
2023-10-19 07:42:55 -0500

    Add files via upload

--- funciones.php
+++ funciones.php
@@ -6,14 +6,8 @@ function consulta(){
 
     $salida=""; //inicializa la variable
     $salida=10*2/2;  //calcula el area del triangulo
-
-
-
-
-
-
-
-
+    $conexion= mysqli_connect("localhost","root","root","db_factura");//conexion hacia la base de datos
+    $sql="select 2+1 as sum";//seleccion de una suma en sql
 
 
 
@@ -26,3 +20,19 @@ function consulta(){
 //retorna la operacion
     return $salida;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
